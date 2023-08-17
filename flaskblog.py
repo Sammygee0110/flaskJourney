48388c994931789c6bd29006729b682355afc316
sammygee0110 <moshebolatansam@gmail.com>
2023-08-17 19:44:07 +0100

    configured the sqlalchemy ORM

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template, url_for, redirect, flash
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "05646efc442447eb4b24b6a554d10d17"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 posts = [
     {
