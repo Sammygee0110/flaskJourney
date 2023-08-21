445be8595de3af09363be6ba6bf01fc3212e1d9f
sammygee0110 <moshebolatansam@gmail.com>
2023-08-21 19:06:55 +0100

    implemented password hashing algorithm

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -7,5 +8,6 @@ app.config["SECRET_KEY"] = "05646efc442447eb4b24b6a554d10d17"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
