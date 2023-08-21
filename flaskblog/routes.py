f9658aa220663ed75e72386d05cbe024cce81ff8
sammygee0110 <moshebolatansam@gmail.com>
2023-08-21 19:40:44 +0100

    fixed database operational error

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -35,6 +35,8 @@ def register():
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        app.app_context().push()
+        db.create_all()
         db.session.add(user)
         db.session.commit()
         flash(f"Account created succesfully for {form.username.data}, you can now login", "success")
