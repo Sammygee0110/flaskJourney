e079eeb055d3418091174db9fb06400fc88e2795
sammygee0110 <moshebolatansam@gmail.com>
2023-08-22 21:17:20 +0100

    fixed account route bug

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,5 +11,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, redirect, flash
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 
 posts = [
@@ -63,6 +63,7 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route("account")
+@app.route("/account")
+@login_required
 def account():
     return render_template('account.html', title='Account')
