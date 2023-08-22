5056030852adbbaf14914fcff094b27ed7bed53b
sammygee0110 <moshebolatansam@gmail.com>
2023-08-22 21:04:41 +0100

    added account page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -62,3 +62,7 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("home"))
+
+@app.route("account")
+def account():
+    return render_template('account.html', title='Account')
