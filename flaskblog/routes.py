b156a2faa0b9d8c77b3039f28d9ca89367ddd2c6
sammygee0110 <moshebolatansam@gmail.com>
2023-08-22 20:41:35 +0100

    implemented dynamic logout routing

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, redirect, flash
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user
 
 
 posts = [
@@ -46,6 +46,8 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -55,3 +57,8 @@ def login():
         else:
             flash("Login failed. Please check email and password!", "danger")
     return render_template("login.html", title="Login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
