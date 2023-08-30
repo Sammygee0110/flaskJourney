21f7bc0bea0e865274a62c05e13e3c0164b05a2e
sammygee0110 <moshebolatansam@gmail.com>
2023-08-30 20:25:13 +0100

    implemented individual post view

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -96,3 +96,8 @@ def new_post():
         flash("Your post has been created succesfully!", "success")
         return redirect(url_for('home'))
     return render_template('new_post.html', title='New Post', form=form)
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
